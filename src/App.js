0f5f61fb439bc99656af2a159b645633226745ee
Hernan Amaiz <hernanamaizp@gmail.com>
2021-09-17 02:55:12 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./perro.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <h1>Dogos Maiz</h1>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Conoce nuestros productos y servicios{" "}
+          <a
+            className="App-link"
+            href="https://maizo21.github.io/veterinaria/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            aqui
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
